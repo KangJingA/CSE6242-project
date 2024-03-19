efa8afdf30b1f16f051d47f2078f20eca5219775
lamppara <76414507+lamppara@users.noreply.github.com>
2024-03-19 21:59:15 +0900

    Rename mapbox_token.py to Dash/mapbox_token.py

---
+++ Dash/mapbox_token.py
@@ -0,0 +1,1 @@
+pk.eyJ1IjoiZGNob2kzMTEiLCJhIjoiY2x0eTh6MHMzMGVzbjJscW43bWpmYnVjZiJ9.SRMzetrrpChjoVGWgWn29g
--- mapbox_token.py
+++
@@ -1,1 +0,0 @@
-pk.eyJ1IjoiZGNob2kzMTEiLCJhIjoiY2x0eTh6MHMzMGVzbjJscW43bWpmYnVjZiJ9.SRMzetrrpChjoVGWgWn29g
