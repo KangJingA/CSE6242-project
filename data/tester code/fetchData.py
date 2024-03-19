c5d9ce64a40572f1730aa120d433fff3ed6f3ba3
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-03-19 22:10:21 +0800

    move code

---
+++ data/tester code/fetchData.py
@@ -0,0 +1,8 @@
+from buses import LTA
+
+apiKey:str = "75cstockTLWN9wEcc9RE6Q=="
+
+lta = LTA(apiKey)
+
+# print(lta.get_passenger_vol_by_bus_stops())
+print(lta.get_passenger_vol_by_OD_bus_stops())
