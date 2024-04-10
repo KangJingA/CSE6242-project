7863581633100977270430a772224a529698cbbf
Nicholas Ang <nicholas.ang@data4life-asia.care>
2024-04-10 19:15:47 +0800

    update demo code

--- data/demo/fetchData.py
+++ data/demo/fetchData.py
@@ -4,16 +4,20 @@ apiKey:str = "75cstockTLWN9wEcc9RE6Q=="
 
 lta = LTA(apiKey)
 
-# retrieves bus stops and saves them in a csv
-max_lines = 10000
+# retrieves all bus routes and saves them in a csv
 csv_file_path = "bus_routes.csv"
 write_headers = False
 with open(csv_file_path, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
     
-    for i in range(0, max_lines, 500):
-        response = lta.get_bus_routes(i)
+    skip = 0
+    while True:
+        response = lta.get_bus_routes(skip)
+        
         bus_routes = response['value']
+        
+        if len(bus_routes) == 0:
+            break
         
         header = bus_routes[0].keys()
         
@@ -23,5 +27,37 @@ with open(csv_file_path, 'w', newline='') as csv_file:
             
         for bus_route in bus_routes: 
             csv_writer.writerow(bus_route.values())
+        
+        print("written " + str(skip + 500) + "lines")
+        skip += 500
     
-    print("csv download complete")
+    print("csv download complete with total of " + str(skip + 500) + "lines")
+    
+    
+csv_file_path = "bus_stops.csv"
+write_headers = False
+with open(csv_file_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    
+    skip = 0
+    while True:
+        response = lta.get_bus_stops2(skip)
+        
+        bus_stops = response['value']
+        
+        if len(bus_stops) == 0:
+            break
+        
+        header = bus_stops[0].keys()
+        
+        if not write_headers:
+            csv_writer.writerow(header)
+            write_headers = True
+            
+        for bus_route in bus_stops: 
+            csv_writer.writerow(bus_route.values())
+        
+        print("written " + str(skip + 500) + "lines")
+        skip += 500
+    
+    print("csv download complete with total of " + str(skip + 500) + "lines")
